820b62249a67f41c1e226f2e62ec7ab8cbb6433b
Eric Stern <egstern@wc.fnal.gov>
2020-10-28 09:33:51 -0500

    Initial set of scripts to build synergia2 devel-pre3 branch on the wc cluster

---
+++ devel-pre3-on-wc/20_install-boost.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+. ./10_create_directories.sh
+
+make_boost=true
+
+if $make_boost
+then
+    cd ${SRC}
+
+    BOOSTSRC=${SRC}/boost
+    if [ -d ${BOOSTSRC} ]
+    then
+        echo "boost already exists.  Won't proceed"
+        exit
+    fi
+
+    BOOSTURL="https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2"
+    wget ${BOOSTURL} -O- | tar xjf -
+    mv boost* boost
+
+
+    if [ ! -d ${BOOSTSRC} ]
+    then
+        echo "boost has not been properly downloaded"
+        exit 10
+    fi
+
+    cd ${BOOSTSRC}
+
+    ./bootstrap.sh --prefix=${SYNINSTALL} --with-python=${ANACONDA_ROOT}/bin/python3.7m --with-libraries=python,regex,filesystem,system,test,serialization,iostreams |& tee bootstrap.out
+
+    export BOOSTBLD=${BLD}/boost
+    mkdir -p ${BOOSTBLD}
+ 
+    ./b2 toolset=gcc cxxflags="-I${ANACONDA_ROOT}/include/python3.7m -std=c++14" --build-dir=${BOOSTBLD} install |& tee b2.out
+
+fi # if make_boost
---
+++ devel-pre3-on-wc/55_install-synergia2.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# ensure directories and environment variables are set
+. ./10_create_directories.sh
+
+if [ -z "${PYTHONPATH}" ]
+then
+    export PYTHONPATH=${SYNINSTALL}/lib:${SYNINSTALL}/lib/${PY_VER}/site-packages
+else
+    export PYTHONPATH=${SYNINSTALL}/lib:${SYNINSTALL}/lib/${PY_VER}/site-packages:${PYTHONPATH}
+fi
+
+export SYNSRC=${SRC}/synergia2
+
+make_synergia2=true
+
+overwrite=0
+reinstall=0
+download=1
+
+if $make_synergia2
+then
+    while [ $# -gt 0 ]
+    do
+        if [ $# -gt 0 -a  "$1" = "reinstall" ]
+        then
+            reinstall=1
+	    echo "reinstall: $reinstall should be 1"
+            shift
+        elif [ $# -gt 0 -a  "$1" = "overwrite" ]
+        then
+            overwrite=1
+	    echo "overwrite: $overwrite should be 1"
+            shift
+	else
+	    echo "arguments can be overwrite or reinstall"
+	    exit 20
+        fi
+    done
+
+    if [ $reinstall -ne 0 ]
+    then
+	echo "Reconfiguring and installing"
+        download=0
+    fi
+    if [ $overwrite -ne 0 ]
+    then
+	echo "overwriting and installing"
+        rm -rf ${SYNSRC}
+        download=1
+    fi
+
+    if [ -d ${SYNSRC} ]
+    then
+	if [ $reinstall -ne 0 ]
+        then
+            download=0
+            echo "reconfiguring existing Synergia source"
+        else
+            echo "synergia2 already exists.  Do:"
+            echo "    $0 reinstall"
+            exit
+        fi
+    else
+        download=1
+    fi
+
+    #exit 20
+    cd ${SRC}
+
+    if [ $download -ne 0 ]
+    then
+        if git clone -b devel-pre3 https://bitbucket.org/fnalacceleratormodeling/synergia2.git |& tee synergia2.git-clone.out
+        then
+            echo "You got synergia2!"
+        else
+            echo "Drat!  Something went wrong downloading synergia2"
+            exit 10
+        fi
+    fi
+
+    if [ ! -d ${SYNSRC} ]
+    then
+        echo "synergia2 has not been properly cloned"
+        exit 10
+    fi
+
+    # work around missing numpy.random.default_rng
+    cp ${SYNSRC}/src/synergia/bunch/tests/CMakeLists.txt ${SYNSRC}/src/synergia/bunch/tests/CMakeLists.txt.before
+    echo "sed command:"
+    sed -i -e '/COMMAND.*diagnostics_crosscheck.py/s/COMMAND.*$/COMMAND cp ${CMAKE_CURRENT_SOURCE_DIR}\/icc-files\/*.icc .\)/' ${SYNSRC}/src/synergia/bunch/tests/CMakeLists.txt
+    echo sed -i -e '/DEPENDS.*diagnostics_crosscheck.py/d' ${SYNSRC}/src/synergia/bunch/tests/CMakeLists.txt
+    sed -i -e '/DEPENDS.*diagnostics_crosscheck.py/d' ${SYNSRC}/src/synergia/bunch/tests/CMakeLists.txt
+
+    SYNBLD=${BLD}/synergia2
+    mkdir -p ${SYNBLD}
+    cd ${SYNBLD}
+
+    if cmake -DBUILD_PYTHON_BINDINGS=1 -DCHEF_DIR=${SYNINSTALL}/lib/chef/cmake -DBOOST_ROOT=${SYNINSTALL} -DGSL_ROOT_DIR=${GSL_DIR} -DFFTW3_INCLUDE_DIR=${FFTW_INC} -DFFTW3_LIBRARIES=${FFTW_DIR}/lib/libfftw3.so -DFFTW3_MPI_LIBRARIES=${FFTW_DIR}/lib/libfftw3_mpi.so -DFFTW3_OMP_LIBRARIES=${FFTW_DIR}/lib/libfftw3_omp.so -DHDF5_ROOT=${HDF5_DIR} -DEIGEN3_INCLUDE_DIR=${SYNINSTALL}/include/eigen3 -DUSE_SIMPLE_TIMER=0 -DUSE_PYTHON_3=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${SYNINSTALL} -DPYTHON_EXECUTABLE=${ANACONDA_ROOT}/bin/python3 ${SYNSRC} |& tee synergia2.cmake.out
+    then
+        echo "synergia2 cmake worked"
+    else
+        echo "Drat!! synergia2 cmake failed"
+        exit 10
+    fi
+
+    if make -j 8 |& tee synergia2.make.out
+    then
+        echo "Congratulations!! synergia2 is make worked !!!"
+    else
+        echo "Drat!! something went wrong building synergia2"
+        exit 10
+    fi
+
+    if  make install |& tee synergia2.install.out
+    then
+        echo "Congratulations!! synergia2 is installed!!!"
+    else
+        echo "Drat!! something went wrong installing synergia2"
+        exit 10
+    fi
+
+fi # if make_synergia2
+
+echo "Define these environment variables:"
+echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+echo "PYTHONPATH=${PYTHONPATH}"
+
+cat >${SYNINSTALL}/bin/setup.sh <<EOF
+#!/bin/bash
+
+module load git
+module load anaconda3
+module load gnu8
+module load openmpi3
+module load fftw
+module load hdf5
+module load cmake
+module load pmix
+module load gsl
+
+PATH=${SYNINSTALL}/bin:\${PATH}
+if [ -n "\${LD_LIBRARY_PATH}" ]
+then
+    export LD_LIBRARY_PATH=${SYNINSTALL}/lib:\${LD_LIBRARY_PATH}
+else
+    export LD_LIBRARY_PATH=${SYNINSTALL}/lib
+fi
+if [ -n "\${PYTHONPATH}" ]
+then
+    export PYTHONPATH=${SYNINSTALL}/lib:${SYNINSTALL}/lib/${PY_VER}/site-packages:\${PYTHONPATH}
+else
+    export PYTHONPATH=${SYNINSTALL}/lib:${SYNINSTALL}/lib/${PY_VER}/site-packages
+fi
+EOF
+
+echo "or source file ${SYNINSTALL}/bin/setup.sh"
