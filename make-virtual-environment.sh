a5344e495ac0eb4a737da986ccf131014aa2cb03
Marc Paterno <paterno@fnal.gov>
2021-03-01 10:12:10 -0600

    User must provide name for new environment

--- make-virtual-environment.sh
+++ make-virtual-environment.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+VENV_NAME=$1
+if [[ -z $VENV_NAME ]]
+then
+  echo "You must specify a name for the new virtual environment"
+  exit 1
+fi
+
 # Make sure to use --system-site-packages, so python can find
 # Homebrew-installed or other system-installed packages (numpy, mpi4py,...)
-python3 -m venv --system-site-packages work-venv|| { echo "Failed to create venv" ; exit 1; }
-source work-venv/bin/activate || { echo "Failed to activate venv"; exit 1; }
+python3 -m venv --system-site-packages ${VENV_NAME} || { echo "Failed to create venv" ; exit 1; }
+source ${VENV_NAME}/bin/activate || { echo "Failed to activate venv"; exit 1; }
 python -m pip install -U pip
-python -m pip install nose
 deactivate
 
 echo "Make sure to source the bin/activate script under this directory"
