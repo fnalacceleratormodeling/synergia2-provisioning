36e3836a4893da90525be8a4578490bef2099de4
Eric G. Stern <egstern@fnal.gov>
2022-01-03 14:31:25 -0600

    added detail instructions for wc cpu

---
+++ devel-3-on-wc/setup-env.sh
@@ -0,0 +1,7 @@
+module load cuda11
+module load gnu9
+module load openmpi3
+source /work1/accelsim/spack-shared-v2/spack/share/spack/setup-env.sh
+export PATH=/work1/accelsim/spack-shared-v2/cmake-3.19.5-Linux-x86_64/bin:$PATH
+
+spack env activate synergia-dev-005
