12a9401d8bfb10ec004cefc89b70e067a1150c0b
Eric G. Stern <egstern@fnal.gov>
2022-04-14 09:07:12 -0500

    utility program to probe nvidia gpu for CMake arguments for building Synergia with Kokkos and CUDA

--- nv_gpu_probe/nv_gpu_probe.cc
+++ nv_gpu_probe/nv_gpu_probe.cc
@@ -67,21 +67,25 @@ int main() {
 #ifdef SM_ONLY
     std::cout << compute_capability;
 #else
+    std::cout << "Add these arguments to the CMake command:" << std::endl;
+    std::cout << "  -DCMAKE_CXX_FLAGS=\"-arch=sm_" ;
+    std::cout << device_properties.major << device_properties.minor;
+    std::cout << " \\" << std::endl;
     switch (compute_capability) {
       // clang-format off
-    case 30: std::cout << "Set -DKokkos_ARCH_KEPLER30=ON ." << std::endl; break;
-    case 32: std::cout << "Set -DKokkos_ARCH_KEPLER32=ON ." << std::endl; break;
-    case 35: std::cout << "Set -DKokkos_ARCH_KEPLER35=ON ." << std::endl; break;
-    case 37: std::cout << "Set -DKokkos_ARCH_KEPLER37=ON ." << std::endl; break;
-    case 50: std::cout << "Set -DKokkos_ARCH_MAXWELL50=ON ." << std::endl; break;
-    case 52: std::cout << "Set -DKokkos_ARCH_MAXWELL52=ON ." << std::endl; break;
-    case 53: std::cout << "Set -DKokkos_ARCH_MAXWELL53=ON ." << std::endl; break;
-    case 60: std::cout << "Set -DKokkos_ARCH_PASCAL60=ON ." << std::endl; break;
-    case 61: std::cout << "Set -DKokkos_ARCH_PASCAL61=ON ." << std::endl; break;
-    case 70: std::cout << "Set -DKokkos_ARCH_VOLTA70=ON ." << std::endl; break;
-    case 72: std::cout << "Set -DKokkos_ARCH_VOLTA72=ON ." << std::endl; break;
-    case 75: std::cout << "Set -DKokkos_ARCH_TURING75=ON ." << std::endl; break;
-    case 80: std::cout << "Set -DKokkos_ARCH_AMPERE80=ON ." << std::endl; break;
+    case 30: std::cout << " -DKokkos_ARCH_KEPLER30=ON \\" << std::endl; break;
+    case 32: std::cout << " -DKokkos_ARCH_KEPLER32=ON \\" << std::endl; break;
+    case 35: std::cout << " -DKokkos_ARCH_KEPLER35=ON \\" << std::endl; break;
+    case 37: std::cout << " -DKokkos_ARCH_KEPLER37=ON \\" << std::endl; break;
+    case 50: std::cout << " -DKokkos_ARCH_MAXWELL50=ON \\" << std::endl; break;
+    case 52: std::cout << " -DKokkos_ARCH_MAXWELL52=ON \\" << std::endl; break;
+    case 53: std::cout << " -DKokkos_ARCH_MAXWELL53=ON \\" << std::endl; break;
+    case 60: std::cout << " -DKokkos_ARCH_PASCAL60=ON \\" << std::endl; break;
+    case 61: std::cout << " -DKokkos_ARCH_PASCAL61=ON \\" << std::endl; break;
+    case 70: std::cout << " -DKokkos_ARCH_VOLTA70=ON \\" << std::endl; break;
+    case 72: std::cout << " -DKokkos_ARCH_VOLTA72=ON \\" << std::endl; break;
+    case 75: std::cout << " -DKokkos_ARCH_TURING75=ON \\" << std::endl; break;
+    case 80: std::cout << " -DKokkos_ARCH_AMPERE80=ON \\" << std::endl; break;
     default:
       std::cout << "Compute capability " << compute_capability
                 << " is not supported" << std::endl;
