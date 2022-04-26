ee1ca26a895eb49e5a3e98c29edb96e55007b306
Eric G. Stern <egstern@fnal.gov>
2022-04-26 15:36:54 -0500

    build for the v100 GPU on almalinux-8

---
+++ devel3-on-almalinux8-v100/10_create_directories.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Create the directories and set environment variables
+
+# The following directory will be the top level of the Synergia build tree
+SYNHOME=${HOME}/syn2-devel3-v100
+
+# load the mpi module
+
+module load mpi
+
+mkdir -p ${SYNHOME}/src
+export SRC=${SYNHOME}/src
+mkdir -p ${SYNHOME}/build
+export BLD=${SYNHOME}/build
+mkdir -p ${SYNHOME}/install
+export SYNINSTALL=${SYNHOME}/install
+
+# put install and cuda directory in paths
+if echo ${PATH} | grep -qv syn2-dev
+then
+    PATH=${SYNINSTALL}/bin:/usr/local/cuda/bin:$PATH
+fi
+
+if echo ${LD_LIBRARY_PATH} | grep -qv syn2-dev
+then
+    if [ -z ${LD_LIBRARY_PATH} ]
+    then
+        export LD_LIBRARY_PATH=${SYNINSTALL}/lib:/usr/local/cuda/lib64
+    else
+        export LD_LIBRARY_PATH=${SYNINSTALL}/lib:/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+    fi
+fi
+
+# the python executable
+PY_EXE=/usr/bin/python3
+
+# the name of the directory under which site-packages will be installed
+export PY_VER=$( ${PY_EXE} -c 'import sys; print("python{}.{}".format(sys.version_info.major, sys.version_info.minor))' )
+echo "python version string is " $PY_VER
---
+++ devel3-on-almalinux8-v100/22_install-fftw.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+. 10_create_directories.sh
+
+make_fftw=true
+
+if $make_fftw
+then
+    cd ${SRC}
+
+    FFTWSRC=${SRC}/fftw
+    if [ -d ${FFTWSRC} ]
+    then
+        echo "fftw already exists.  Won't proceed"
+        exit
+    fi
+
+    FFTWURL="https://www.fftw.org/fftw-3.3.10.tar.gz"
+    curl --insecure "${FFTWURL}" | tar xzvf -
+    mv fftw-3* fftw
+
+
+    if [ ! -d ${FFTWSRC} ]
+    then
+        echo "fftw has not been properly downloaded"
+        exit 10
+    fi
+
+    cd ${FFTWSRC}
+    scl enable gcc-toolset-9 './configure --prefix=${SYNINSTALL} --disable-fortran --enable-mpi --enable-openmp --enable-shared --disable-dependency-tracking'  |& tee fftw3_configure.out
+    scl enable gcc-toolset-9 make |& tee fftw3_make.out
+    scl enable gcc-toolset-9 'make install' |& tee fftw3_makeinstall.out
+
+fi # if make_fftw
+
