3f3c54dd593e1bee623a922ef61643c311298906
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2023-04-11 10:49:03 -0500

    updates

--- devel-3-on-wc/devel3-v100/10_create_directories.sh
+++ devel-3-on-wc/devel3-v100/10_create_directories.sh
@@ -3,9 +3,10 @@
 
 module purge > /dev/null 2>&1
 module load git
-module load gnu11
+module load gnu12
+module load cuda12
 
-source /wclustre/accelsim/spack-shared-v3/setup_env_synergia-devel3-v100-001.sh
+source /wclustre/accelsim/spack-shared-v4/setup_env_synergia-devel3-v100-002.sh
 
 # The following directory will be the top level of the Synergia build tree
 #SYNHOME=${HOME}/syn2-devel3-v100
