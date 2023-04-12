e5d1790d608a64e35c3716bfcba66d7054436ec4
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2023-04-12 12:41:00 -0500

    updates

--- devel-3-on-wc/devel3-v100/submit_wrapper_ompi.sh
+++ devel-3-on-wc/devel3-v100/submit_wrapper_ompi.sh
@@ -11,10 +11,9 @@
 
 module purge > /dev/null 2>&1
 module load git
-module load gnu12
-module load cuda12
+module load gnu11
+module load cuda11
 
 source /wclustre/accelsim/spack-shared-v4/setup_env_synergia-devel3-v100-002.sh
 
-
-srun --mpi=pmix_v3 ./wrapper_ompi.sh 
+mpirun -np 1 ./wrapper_ompi.sh 
